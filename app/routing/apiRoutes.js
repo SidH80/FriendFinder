cf52b8be9810a9b0ab1ead26fee8b45ad3e6bbef
lethalSid <senien.hicks@gmail.com>
2019-10-08 17:28:28 -0400

    grabbed the data from the client side

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,34 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         //send all information here and do calculations
+        var userData = req.body;
+        var scores = userData.scores
+        totalScore = [];
+
+        var userAvg;
+        var friendAvg;
+        //console.log(userData.scores);
+
+        for (let i = 0; i < scores.length; i++) {
+            totalScore.push(parseInt(scores[i]));
+        }
+
+        console.log(totalScore);
+        //friends.push(userData);
+        //console.log(friends);
+        for (let i = 0; i < friends.length; i++) {
+            console.log(friends[i].name, friends[i].scores)
+        }
+
+
 
     })
+
+    //compare userData score variance against the score of all other friends scores in the database (friends-variable)
+    //calculate the score of each friend and push it to an array - keep the friend name
+    //
+
+
 
 
 }

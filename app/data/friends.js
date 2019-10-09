0d4cb4dc32efed0a6771d10c34ba717efcea07cc
lethalSid <senien.hicks@gmail.com>
2019-10-09 18:12:57 -0400

    added random user photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,38 @@
 //change the friends data
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Amy",
+        "photo":"https://randomuser.me/api/portraits/women/4.jpg",
         "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
         },
         {
         "name":"Sarah",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://randomuser.me/api/portraits/women/29.jpg",
         "scores":[ 1, 1, 1, 1, 1, 5, 5, 5, 4, 2]
         },
         {
         "name":"Miller",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://randomuser.me/api/portraits/men/28.jpg",
         "scores":[ 1, 3, 2, 2, 1, 1, 2, 2, 3, 1]
         },
         {
-        "name":"Mikey",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Molly",
+        "photo":"https://randomuser.me/api/portraits/women/29.jpg",
         "scores":[ 3, 3, 3, 1, 2, 1, 2, 5, 5, 3]
         },
         {
         "name":"Wayne",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://randomuser.me/api/portraits/men/56.jpg",
         "scores":[ 5, 5, 5, 5, 5, 4, 4, 4, 4, 1]
         },
         {
         "name":"Lenny",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://randomuser.me/api/portraits/women/25.jpg",
         "scores":[ 1, 2, 2, 3, 3, 4, 4, 5, 1, 1]
         },
         {
-        "name":"Millie",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Ann",
+        "photo":"https://randomuser.me/api/portraits/women/88.jpg",
         "scores":[ 1, 2, 3, 4, 5, 4, 3, 2, 1, 4]
         }
 ];

d71d35970893420c57c7448899a55f56c9846da6
lethalSid <senien.hicks@gmail.com>
2019-10-09 07:16:44 -0400

    calculated the difference between scores for each question for each user and friend in the array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,30 +13,32 @@ module.exports = function(app) {
         var scores = userData.scores
         totalScore = [];
 
-        var userAvg;
-        var friendAvg;
         //console.log(userData.scores);
 
         for (let i = 0; i < scores.length; i++) {
             totalScore.push(parseInt(scores[i]));
         }
+        //console.log(totalScore);
+        for (let j = 0; j < friends.length; j++) {
+            //console.log(friends[j].name, friends[j].scores)
+            var totalDifference = totalScore.map(
+                function(a, i) {
+                     return Math.abs(a - parseInt(friends[j].scores));
+                    });
+            console.log(`${friends[j].name}'s total diff from you is is ${totalDifference.reduce((a,b) => a + b, 0)}`);
 
-        console.log(totalScore);
-        //friends.push(userData);
-        //console.log(friends);
-        for (let i = 0; i < friends.length; i++) {
-            console.log(friends[i].name, friends[i].scores)
         }
 
 
+        //console.log(totalScore);
+        //friends.push(userData);
+        //console.log(friends);
+        //let friendsScores = totalScore.scores;
 
     })
 
     //compare userData score variance against the score of all other friends scores in the database (friends-variable)
     //calculate the score of each friend and push it to an array - keep the friend name
     //
-
-
-
 
 }
